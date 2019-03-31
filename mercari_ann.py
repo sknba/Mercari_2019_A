43dd847364f1a4c005fc5f395daa3253f0190f7b
TomekQ13 <kuczak.tomasz@gmail.com>
2019-03-31 23:25:59 +0200

    Preprocessing po dodaniu ceny do train setu i poczatek prac nad ANN

--- mercari_ann.py
+++ mercari_ann.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras import optimizers
 import pandas as pd
 import numpy as np
 
@@ -15,17 +16,17 @@ regressor.add(Dropout(rate = 0.1))
 regressor.add(Dense(units = 250, kernel_initializer = 'uniform', activation = 'relu'))
 regressor.add(Dropout(rate = 0.1))
 regressor.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'relu'))
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['mean_squared_error'])
+regressor.compile(optimizer = 'sgd', loss = 'mean_squared_error', metrics = ['mean_absolute_error'])
 
 #training the ANN
 chunk_number = 0
-for training_set, dataset in zip(pd.read_csv('train_preprocessed.csv', chunksize = 10000),
-                                 pd.read_csv('train_no_header.csv', chunksize = 10000)):
-    print('Chunk number: ' & str(chunk_number) & ' Number of chunks ~150')
-    X_train = training_set.iloc[:, :].values
-    y_train = dataset.iloc[:, 5].values
-    regressor.fit(X_train, y_train, batch_size = 64, epochs = 50)
 
+for training_set in pd.read_csv('train_preprocessed.csv', chunksize = 10000):                                 
+    print('Chunk number: ' + str(chunk_number) + ' Number of chunks ~150')
+    X_train = training_set.iloc[:, :-1].values
+    y_train = training_set.iloc[:, -1].values
+    regressor.fit(X_train, y_train, batch_size = 64, epochs = 50)
+    chunk_number = chunk_number + 1
 
 """Sieć działa, kod na na dole ma służyć do dobrania najlepszych parametrów dla niej
 To jest dosc proste, ale wymaga sporo czasu i wolnej mocy obliczeniowej :D
--- mercari_preprocessing.py
+++ mercari_preprocessing.py
@@ -1,17 +1,20 @@
 #przed wykonaniem kodu upewnij się, że masz około 20 GB miejsca na dysku
 #zajmie to prawdopodobnie około 1,5h
 #importing necessary libraries
+
 import pandas as pd
 import numpy as np
-from scipy import sparse
 from sklearn.preprocessing import LabelEncoder
 from dummyPy import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
 
 #creating classes
 labelencoder_c1 = LabelEncoder()
 labelencoder_c2 = LabelEncoder()
 labelencoder_c3 = LabelEncoder()
 ohe = OneHotEncoder(['category_1', 'category_2', 'category_3'])
+sc = StandardScaler()
 
 #fitting the label encoder
 d1 = pd.read_csv('train.tsv', sep = '\t')
@@ -29,7 +32,7 @@ labelencoder_c3.fit(category_list['category_3'])
 
 
 #transforming the training set to fit the onehotencoder
-d1 = d1.drop('price', axis = 1)
+#d1 = d1.drop('price', axis = 1)
 d1['category_name'] = d1['category_name'].astype('str')
 d1['category_name'] = d1['category_name'].replace('nan', 'no_category1/no_category2/no_category3')
 categories = d1['category_name'].str.split('/', 3, expand = True)
@@ -39,11 +42,12 @@ d1['category_3'] = categories[2]
 d1['category_1'] = labelencoder_c1.transform(d1['category_1'])
 d1['category_2'] = labelencoder_c2.transform(d1['category_2'])
 d1['category_3'] = labelencoder_c3.transform(d1['category_3'])
-d1_filtered = d1[['item_condition_id',
-                    'shipping',
-                    'category_1',
-                    'category_2',
-                    'category_3']].copy()
+d1_filtered = d1[['price',
+                  'item_condition_id',
+                  'shipping',
+                  'category_1',
+                  'category_2',
+                  'category_3']]
 ohe.fit(d1_filtered)
 
 
@@ -61,36 +65,43 @@ d2_filtered = d2[['item_condition_id',
                     'shipping',
                     'category_1',
                     'category_2',
-                    'category_3']].copy()
+                    'category_3']]
 ohe.fit(d2_filtered)
 
 del categories, category_list, d1, d2, d1_filtered, d2_filtered
 
 
-#preprocessing the training set
-for dataset in pd.read_csv('train.tsv', sep = '\t', chunksize = 10000):    
+#preprocessing the training set (148 chunks) (.copy() removed)
+i = 0
+for dataset in pd.read_csv('train.tsv', sep = '\t', chunksize = 10000):
+    print(i)    
     dataset['category_name'] = dataset['category_name'].astype('str')
     dataset['category_name'] = dataset['category_name'].replace('nan', 'no_category1/no_category2/no_category3')
     categories = dataset['category_name'].str.split('/', 3, expand = True)
     dataset['category_1'] = categories[0]
     dataset['category_2'] = categories[1]
     dataset['category_3'] = categories[2]
-    dataset_filtered = dataset[['item_condition_id',
+    dataset_filtered = dataset[['price',
+                            'item_condition_id',
                             'shipping',
                             'category_1',
                             'category_2',
-                            'category_3']].copy()
+                            'category_3']]
     dataset_filtered['category_1'] = labelencoder_c1.transform(dataset_filtered['category_1'])
     dataset_filtered['category_2'] = labelencoder_c2.transform(dataset_filtered['category_2'])
     dataset_filtered['category_3'] = labelencoder_c3.transform(dataset_filtered['category_3'])
     dataset_filtered = ohe.transform(dataset_filtered)
+   # dataset_filtered = pd.concat([dataset_filtered, dataset['price']], axis = 1, ignore_index = True)
     dataset_filtered.to_csv('train_preprocessed.csv', mode = 'a', index = False, header = False)
+    i = i + 1
 #test = pd.read_csv('train_preprocessed.csv')
 #test = test.head(11000)
     
     
-#preprocessing the test set
+#preprocessing the test set (probably around 450 chunks)
+j = 0
 for dataset_test in pd.read_csv('test_stg2.tsv', sep = '\t', chunksize = 10000):
+    print(j)
     dataset_test['category_name'] = dataset_test['category_name'].astype('str')
     dataset_test['category_name'] = dataset_test['category_name'].replace('nan', 'no_category1/no_category2/no_category3')
     categories_test = dataset_test['category_name'].str.split('/', 3, expand = True)
@@ -101,39 +112,18 @@ for dataset_test in pd.read_csv('test_stg2.tsv', sep = '\t', chunksize = 10000):
                                 'shipping',
                                 'category_1',
                                 'category_2',
-                                'category_3']].copy()
+                                'category_3']]
     dataset_test_filtered['category_1'] = labelencoder_c1.transform(dataset_test_filtered['category_1'])
     dataset_test_filtered['category_2'] = labelencoder_c2.transform(dataset_test_filtered['category_2'])
     dataset_test_filtered['category_3'] = labelencoder_c3.transform(dataset_test_filtered['category_3'])
     dataset_test_filtered = ohe.transform(dataset_test_filtered)
     dataset_test_filtered.to_csv('test_stg2_preprocessed.csv', mode = 'a', index = False, header = False)
+    j = j + 1
+del categories, categories_test, dataset, dataset_filtered, dataset_test, dataset_test_filtered
 
-"""od tego momentu jeszcze nie działa
-
-#Preparing X and y
-#Sparse matrix
-######################3
-from scipy import sparse
-y_train = dataset_final.iloc[:, 2].values
-dataset_final = dataset_final.drop('price', axis = 1)
-X_sparse = sparse.csr_matrix(dataset_final.values)
-X_sparse_test = sparse.csr_matrix(dataset_test_final)
-
-# Feature Scaling
-######################
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_sparse = sc.fit_transform(X_sparse)
-X_sparse_test = sc.transform(X_sparse_test)
-
-#Fitting Linear Regression
-#############################
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_sparse, y_train)
-regressor.score(X_sparse, y_train)
-
-#Predicting the results of the test set
-##########################################
-y_pred = regressor.predict(X_sparse_test)
+#testing ground
+nans = d1['price'][d1['price'] == 'nan']
+sample = pd.read_csv('train_preprocessed3.csv', nrows = 1001)
+nulls = sample.isnull().values
+rows_null = sample.iloc[:, :][nulls]
 """
